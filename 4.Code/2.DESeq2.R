1c14e1ca6189a57088b6c0c49643d11346fbe190
ys910111 <ys910111@snu.ac.kr>
2022-03-16 17:40:10 +0900

    Update 2.DESeq2.R

--- 4.Code/2.DESeq2.R
+++ 4.Code/2.DESeq2.R
@@ -1,13 +1,18 @@
+setwd("C:/Users/user/Documents/GitHub/Education_BulkRNAseq/")
 library("DESeq2")
 # Input : cts <- count data, coldata <- coldata
+counts <- read.table("1.Data/count_table.txt", sep = "\t", header = T)
+coldata <- read.table("1.Data/group_table.txt", sep = "\t", header = T)
 
 # PCA Plot
-SummarizedExperiment(assays = cts, colData = coldata)
-
+# SummarizedExperiment(assays = counts, colData = coldata)
 
 # DEG analysis
+## Divide by day
+
 ## Make DESeq Dataset
-dds <- DESeqDataSetFromMatrix(countData = cts,
+
+dds <- DESeqDataSetFromMatrix(countData = counts,
                               colData = coldata,
                               design = ~)
 
