3ea8206d4c6028cb7a9645c795fda517d63e1f2a
Khreat0205 <scientist0205@snu.ac.kr>
2022-03-17 10:34:47 +0900

    kh

--- 0.Pre-install/installRpackages.R
+++ 0.Pre-install/installRpackages.R
@@ -1,5 +1,8 @@
 install.packages("data.table")
 install.packages("BiocManager")
+install.packages('rmarkdown')
+install.packages('tinytex')
 
 BiocManager::install("biomaRt")
 BiocManager::install("DESeq2")
+
