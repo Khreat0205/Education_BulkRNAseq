f3845423a6c2fe6722107b759952f67ca8ab6ab3
Khreat0205 <scientist0205@snu.ac.kr>
2022-04-14 10:45:08 +0900

    update

---
+++ 2.Practice_code/0.R_Rstudio.R
@@ -0,0 +1,18 @@
+x <- c("a","b","c")
+x
+z <-c(1,2,3,4,5,6,7,8,9,10)
+z
+i <- c("1","2","3","4","5")
+i
+j <- c("a",1,2)
+
+
+coldata <- read.table("1.Data/group_table.txt", sep = "\t", header = T)
+countdata <- read.table("1.Data/count_table.txt", sep = "\t", header = T)
+countdata
+?read.table
+head(coldata)
+?head
+coldata[1,1]
+coldata[1,2]
+coldata[2,2]
